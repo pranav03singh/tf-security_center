fd70361c628215de14e46c2841cefdb808087e31
Pranav Singh <pranav03singh@outlook.com>
2021-10-07 22:30:43 +0530

    updated

--- output.tf
+++ output.tf
@@ -1,8 +1,3 @@
-output "security_center_subscription_pricing_id" {
-  description = "The subscription pricing ID"
-  value       = azurerm_security_center_subscription_pricing.main.id
-}
-
 output "security_center_contact_id" {
   description = "The Security Center Contact ID"
   value       = azurerm_security_center_contact.main.id
