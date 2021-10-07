b2edc23c401270343cc7e8de0de9308069563a1c
Pranav Singh <pranav03singh@outlook.com>
2021-10-07 17:24:56 +0530

    updated

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "security_center_workspace_id" {
   description = "The Security Center Workspace resource ID."
-  value       = azurerm_security_center_workspace.main.id
+  value       = azurerm_security_center_workspace.main[count.index].id
 }
 
 output "security_center_subscription_pricing_id" {
