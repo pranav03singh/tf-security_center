745562eb8ff0ac1ff889e5bbcc432404e4efc78d
Pranav Singh <pranav03singh@outlook.com>
2021-10-07 16:43:00 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 #----------------------------------------------------------
+# Generate Random Integer
+#----------------------------------------------------------
+resource "random_integer" "random_value" {
+  min = 100
+  max = 50000
+}
+
+
+#----------------------------------------------------------
 # Resource Group, Log Analytics Data Resources
 #----------------------------------------------------------
 data "azurerm_resource_group" "rg" {
@@ -16,13 +25,42 @@ data "azurerm_log_analytics_workspace" "logws" {
 
 data "azurerm_subscription" "current" {}
 
+#---------------------------------------------------------
+# Resource Group Creation or selection - Default is "false"
+#----------------------------------------------------------
+
+data "azurerm_resource_group" "rgrp" {
+  count = var.create_resource_group == false ? 1 : 0
+  name  = var.resource_group_name
+}
+
+resource "azurerm_resource_group" "rg" {
+  count    = var.create_resource_group ? 1 : 0
+  name     = var.resource_group_name
+  location = var.resource_group_location
+  tags     = merge({ "Name" = format("%s", var.resource_group_name) }, var.tags, )
+}
+
+
+#----------------------------------------------------------
+# Log Analytics Workspace Resource
+#----------------------------------------------------------
+resource "azurerm_log_analytics_workspace" "main" {
+  count               = var.log_analytics_workspace_name ? 0 : 1
+  name                = "log-ws-"+random_integer.random_value
+  location            = var.rg_location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
 #----------------------------------------------------------
 # Azure Security Center Workspace Resource
 #----------------------------------------------------------
 
 resource "azurerm_security_center_workspace" "main" {
   scope        = var.scope_resource_id == null ? data.azurerm_subscription.current.id : var.scope_resource_id
-  workspace_id = data.azurerm_log_analytics_workspace.logws.id
+  workspace_id = azurerm_log_analytics_workspace.main.id
 }
 
 #----------------------------------------------------------
--- variables.tf
+++ variables.tf
@@ -1,6 +1,18 @@
+variable "create_resource_group" {
+  description = "Whether to create resource group and use it for all networking resources"
+  type        = bool
+  default     = true
+}
+
 variable "resource_group_name" {
   description = "A container that holds related resources for an Azure solution"
   default     = ""
+}
+
+variable "resource_group_location" {
+  description = "Location for Resource Group"
+  type        = string
+  default     = "eastus"
 }
 
 variable "log_analytics_workspace_name" {
